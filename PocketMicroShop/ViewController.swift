3248ad4703fcee9cdc7c4f6643d63b26a805f824
ff_pinyao <xiaobin.fan@lovepinyao.com>
2016-12-19 11:29:38 +0800

    Initial Commit

---
+++ PocketMicroShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PocketMicroShop
+//
+//  Created by lovepinyao on 16/12/19.
+//  Copyright © 2016年 lovepinyao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
